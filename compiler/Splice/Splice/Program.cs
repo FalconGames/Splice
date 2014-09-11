21b889c5794ebf510ac49c5291b4168643e905da
FalconGames <falcongames109@gmail.com>
2014-09-11 00:22:19 -0400

    Added lots of info
    Finished soup description and examples and added node, module, and freeze/melt descriptions

---
+++ compiler/Splice/Splice/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Splice
+{
+	class Program
+	{
+		static void Main(string[] args)
+        {
+			string code =
+@"void main()
+{
+	print(""Hello"");
+}";
+			Console.WriteLine(code);
+			Console.ReadKey();
+		}
+	}
+}
